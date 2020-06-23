a2525d41047ff80b84531048790e3b21b8f0e6d1
Guillaume R <guillaume.rondon@gmail.com>
2020-06-23 22:47:40 +0200

    Taking and giving ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,5 +1,16 @@
 fn main() {
-    let mut s = String::from("hello");
-    s.push_str(", world!");
-    println!("{}", s);
+    let s1 = gives_ownership();
+    let s2 = String::from("hello");
+    let s3 = takes_and_gives_back(s2);
 }
+
+fn gives_ownership() -> String {
+    let some_string = String::from("hello");
+    some_string
+}
+
+fn takes_and_gives_back(a_string: String) -> String {
+    a_string
+}
+
+
