e47b7e5393246f2d864867385a04ceb8368cab1b
Guillaume R <guillaume.rondon@gmail.com>
2020-06-24 21:11:26 +0200

    Katas from codewars

---
+++ kata/src/main.rs
@@ -0,0 +1,66 @@
+fn main() {
+    println!("Hello, world!");
+    let bmi = bmi(50, 1.80);
+    println!("bmi {}", bmi);
+    let bounces = bouncing_ball(3.0, 0.66, 1.5);
+    println!("bounces {}", bounces);
+}
+
+
+fn bmi(weight: u32, height: f32) -> &'static str {
+    let bmi: f32 = weight as f32 / (height * height);
+    match bmi {
+        b if b <= 18.5 => "Underweight",
+        b if b <= 25.0 => "Normal",
+        b if b <= 30.0 => "Overweight",
+        b if b > 30.0 => "Obese",
+        _ => "",
+    }
+}
+
+fn bouncing_ball(h: f64,  bounce: f64,  window: f64) -> i32 {
+    if h < 0.0 {
+        return -1;
+    }
+
+    if bounce <= 0.0 || bounce >= 1.0 {
+        return -1;
+    }
+
+    if window >= h {
+        return -1
+    } 
+
+    let mut nb_views: i32 = 1;
+
+    let mut remaining_h: f64 = h * bounce;
+    while remaining_h > window {
+        nb_views += 2;
+        remaining_h = remaining_h * bounce;
+    }
+    nb_views
+}
+
+
+#[test]
+    fn basic_tests() {
+        assert_eq!(bmi(50, 1.80), "Underweight");
+        assert_eq!(bmi(80, 1.80), "Normal");
+        assert_eq!(bmi(90, 1.80), "Overweight");
+        assert_eq!(bmi(110, 1.80), "Obese");
+    }
+
+
+    fn testequal(h: f64,  bounce: f64,  window: f64, exp: i32) -> () {
+        assert_eq!(bouncing_ball(h, bounce, window), exp)
+    }
+    
+    #[test]
+    fn tests_bouncing_ball() {
+    
+        testequal(3.0, 0.66, 1.5, 3);
+        testequal(30.0, 0.66, 1.5, 15);
+        testequal(40.0, 0.4, 10.0, 3);
+        testequal(10.0, 0.6, 10.0, -1);
+      
+    }
--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -2,6 +2,12 @@ fn main() {
     let s1 = gives_ownership();
     let s2 = String::from("hello");
     let s3 = takes_and_gives_back(s2);
+    let mut s4 = String::from("hello mutation");
+    change(&mut s4);
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(" land");
 }
 
 fn gives_ownership() -> String {
