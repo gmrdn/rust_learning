fe5a879b1623abac74e7b310cf464ba546a1ffd1
Guillaume R <guillaume.rondon@gmail.com>
2020-07-01 19:49:53 +0200

    Using a workspace Cargo.toml to manage multiple packages

--- slices/src/main.rs
+++ slices/src/main.rs
@@ -1,17 +1,17 @@
 fn main() {
     println!("Hello, world!");
     println!("first word: {}", first_word(&String::from("Lorem ipsum et blabla bla etc.")));
+    println!("string slice : {}", &String::from("string slice")[0..5]);
 }
 
 
 fn first_word(s: &String) -> usize {
-    let bytes = s.as_bytes();
+    let bytes: &[u8] = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
             return i;
         }
     }
-
     s.len()
 }
